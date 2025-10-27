99b91b3e3540075e4f43632c4ff726a60fcd5e6c
Michal Pokrywka <wolfmoon@o2.pl>
2025-10-27 21:50:01 +0100

    WStompConfig builder

--- src/connect.rs
+++ src/connect.rs
@@ -1,21 +1,22 @@
 use actix_http::Uri;
 use async_stomp::client::Connector;
 use awc::{
-    Client,
     error::{HttpError, WsClientError},
     ws::WebsocketsRequest,
 };
 
-use crate::{WStompClient, WStompConnectError};
+use crate::{WStompClient, WStompConfig, WStompConnectError};
 
+/// Connect to STOMP server without additional parameters
 pub async fn connect<U>(url: U) -> Result<WStompClient, WStompConnectError>
 where
     Uri: TryFrom<U>,
     <Uri as TryFrom<U>>::Error: Into<HttpError>,
 {
-    connect_with_options(Client::default(), url, vec![], None, None).await
+    WStompConfig::new(url).build_and_connect().await
 }
 
+/// Connect to STOMP server using authorization token
 pub async fn connect_with_token<U>(
     url: U,
     auth_token: impl Into<String>,
@@ -24,73 +25,27 @@ where
     Uri: TryFrom<U>,
     <Uri as TryFrom<U>>::Error: Into<HttpError>,
 {
-    connect_with_options(
-        Client::default(),
-        url,
-        headers_for_token(auth_token),
-        None,
-        None,
-    )
-    .await
+    WStompConfig::new(url)
+        .auth_token(auth_token)
+        .build_and_connect()
+        .await
 }
 
+/// Connect to STOMP server using password
 pub async fn connect_with_pass<U>(
     url: U,
-    login: String,
-    passcode: String,
+    login: impl Into<String>,
+    passcode: impl Into<String>,
 ) -> Result<WStompClient, WStompConnectError>
 where
     Uri: TryFrom<U>,
     <Uri as TryFrom<U>>::Error: Into<HttpError>,
 {
-    connect_with_options(Client::default(), url, vec![], Some(login), Some(passcode)).await
-}
-
-pub async fn connect_with_options<U>(
-    client: Client,
-    url: U,
-    headers: Vec<(String, String)>,
-    login: Option<String>,
-    passcode: Option<String>,
-) -> Result<WStompClient, WStompConnectError>
-where
-    Uri: TryFrom<U>,
-    <Uri as TryFrom<U>>::Error: Into<HttpError>,
-{
-    let uri = Uri::try_from(url).map_err(|e| {
-        let err: HttpError = e.into();
-        WStompConnectError::WsClientError(WsClientError::from(err))
-    })?;
-
-    let (authority, host_name) = uri
-        .authority()
-        .map(|a| (a.to_string(), a.host().to_string()))
-        .unwrap_or_default();
-
-    let stomp_client = client.ws::<Uri>(uri).stomp_connect().await?;
-
-    let connect_msg = Connector::builder()
-        .server(authority.clone())
-        .virtualhost(authority)
-        .headers(headers)
-        .use_tls(true)
-        .tls_server_name(host_name);
-
-    let connect_msg = if let Some(login) = login
-        && let Some(passcode) = passcode
-    {
-        connect_msg.login(login).passcode(passcode).msg()
-    } else {
-        connect_msg.msg()
-    };
-
-    stomp_client
-        .send(connect_msg)
+    WStompConfig::new(url)
+        .login(login)
+        .passcode(passcode)
+        .build_and_connect()
         .await
-        .inspect_err(|err| println!("CONNECT error: {err}"))
-        .map_err(WStompConnectError::ConnectMessageFailed)?;
-
-    Ok(stomp_client)
 }
 
 pub trait StompConnect {
@@ -107,7 +62,71 @@ impl StompConnect for WebsocketsRequest {
             .await
             .map_err(WStompConnectError::WsClientError)?;
 
-        Ok(WStompClient::new(framed_connection))
+        Ok(WStompClient::from_framed(framed_connection))
+    }
+}
+
+impl<U> WStompConfig<U> {
+    pub async fn build_and_connect(self) -> Result<WStompClient, WStompConnectError>
+    where
+        Uri: TryFrom<U>,
+        <Uri as TryFrom<U>>::Error: Into<HttpError>,
+    {
+        let (url, opts) = self.into_inner();
+
+        let client = if let Some(client) = opts.client {
+            client
+        } else {
+            #[cfg(feature = "rustls")]
+            if opts.ssl {
+                crate::connect_ssl::create_ssl_client()
+            } else {
+                awc::Client::default()
+            }
+            #[cfg(not(feature = "rustls"))]
+            awc::Client::default()
+        };
+
+        let uri = Uri::try_from(url).map_err(|e| {
+            let err: HttpError = e.into();
+            WStompConnectError::WsClientError(WsClientError::from(err))
+        })?;
+
+        let (authority, host_name) = uri
+            .authority()
+            .map(|a| (a.to_string(), a.host().to_string()))
+            .unwrap_or_default();
+
+        let mut headers = opts.additional_headers;
+
+        if let Some(auth_token) = opts.auth_token {
+            headers.extend(headers_for_token(auth_token));
+        }
+
+        let stomp_client = client.ws::<Uri>(uri).stomp_connect().await?;
+
+        let connect_msg = Connector::builder()
+            .server(authority.clone())
+            .virtualhost(authority)
+            .headers(headers)
+            .use_tls(true)
+            .tls_server_name(host_name);
+
+        let connect_msg = if let Some(login) = opts.login
+            && let Some(passcode) = opts.passcode
+        {
+            connect_msg.login(login).passcode(passcode).msg()
+        } else {
+            connect_msg.msg()
+        };
+
+        stomp_client
+            .send(connect_msg)
+            .await
+            .inspect_err(|err| println!("CONNECT error: {err}"))
+            .map_err(WStompConnectError::ConnectMessageFailed)?;
+
+        Ok(stomp_client)
     }
 }
 
